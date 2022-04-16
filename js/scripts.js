b6f730e9de69764c40cdae8fd39761442181331b
Kyle Frame <kjframe1223@gmail.com>
2022-04-16 11:08:31 -0500

    Login & Reserve Campsite buttons functional via JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 src="node_modules/jquery/dist/jquery.slim.min.js"
+
+$(function() {
+    $("#reserveButton").click(function() {
+        $("#reserveModal").modal("show");
+    });
+
+    $("#loginButton").click(function() {
+        $("#loginModal").modal("show");
+    })
+});
