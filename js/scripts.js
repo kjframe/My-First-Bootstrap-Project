c9bf107c40c3d986c556bda55eb8eb1b232ef9a6
Kyle Frame <kjframe1223@gmail.com>
2022-04-16 11:30:41 -0500

    Modal design in styles.scss

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 src="node_modules/jquery/dist/jquery.slim.min.js"
 
+//Login and Reserve Campsite buttons
 $(function() {
     $("#reserveButton").click(function() {
         $("#reserveModal").modal("show");
